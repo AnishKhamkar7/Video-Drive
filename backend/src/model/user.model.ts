eef9080b6c23933b8b7a2651ad6aff145e5cd941
AnishKhamkar7 <anishkhamkar7@gmail.com>
2024-10-04 17:17:57 +0530

    [Backend] feat: added user schema to models

---
+++ backend/src/model/user.model.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
